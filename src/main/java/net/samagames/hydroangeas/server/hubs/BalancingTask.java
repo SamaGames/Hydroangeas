c7849a912de26103283f6d6e7e31f35b945a6b9f
Silvanosky <geekpower14@gmail.com>
2015-10-11 22:35:04 +0200

    Change check time for the hub balancer

--- src/main/java/net/samagames/hydroangeas/server/hubs/BalancingTask.java
+++ src/main/java/net/samagames/hydroangeas/server/hubs/BalancingTask.java
@@ -50,7 +50,7 @@ public class BalancingTask extends Thread
                         }
                     }
                 }
-                Thread.sleep(1000);
+                Thread.sleep(3000);
             } catch (InterruptedException e)
             {
                 e.printStackTrace();
