d4e438d16ff36d11a1fbeef284dfe077b9777dd7
Silvanosky <geekpower14@gmail.com>
2015-10-17 21:13:16 +0200

    Add channel for hub listener channel

--- src/main/java/net/samagames/hydroangeas/server/HydroangeasServer.java
+++ src/main/java/net/samagames/hydroangeas/server/HydroangeasServer.java
@@ -52,6 +52,7 @@ public class HydroangeasServer extends Hydroangeas
             }
         });
         this.redisSubscriber.registerReceiver("serverUpdateChannel", new ServerStatusReceiver(this));
+        this.redisSubscriber.registerReceiver("hubUpdateChannel", new ServerStatusReceiver(this));
         this.redisSubscriber.registerReceiver("hubsChannel", new ServerStatusReceiver(this));
 
         this.clientManager = new ClientManager(this);
