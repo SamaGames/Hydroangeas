1280327a9e001360bf45baef645b66f170edf83d
Silvanosky <geekpower14@gmail.com>
2015-11-07 11:15:48 +0100

    Add authentification url compability

--- src/main/java/net/samagames/hydroangeas/utils/NetworkUtils.java
+++ src/main/java/net/samagames/hydroangeas/utils/NetworkUtils.java
@@ -2,8 +2,11 @@ package net.samagames.hydroangeas.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
 
 public class NetworkUtils
 {
@@ -12,10 +15,19 @@ public class NetworkUtils
         try
         {
             URL url = new URL(rawURL);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            URLConnection urlConnection = url.openConnection();
+
+            if (url.getUserInfo() != null) {
+                String basicAuth = "Basic " + new String(Base64.getEncoder().encode(url.getUserInfo().getBytes()));
+                urlConnection.setRequestProperty("Authorization", basicAuth);
+            }
+
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 
             String inputLine = in.readLine();
             in.close();
+            inputStream.close();
 
             return inputLine;
         } catch (IOException e)
