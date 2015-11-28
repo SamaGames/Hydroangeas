7bab02b437973638a5e1f7923a349e768de0ddfa
Silvanosky <geekpower14@gmail.com>
2015-11-28 21:07:46 +0100

    Shutdown server by template

--- src/main/java/net/samagames/hydroangeas/server/commands/ShutdownCommand.java
+++ src/main/java/net/samagames/hydroangeas/server/commands/ShutdownCommand.java
@@ -4,6 +4,9 @@ import net.samagames.hydroangeas.common.commands.AbstractCommand;
 import net.samagames.hydroangeas.server.HydroangeasServer;
 import net.samagames.hydroangeas.server.client.HydroClient;
 import net.samagames.hydroangeas.server.client.MinecraftServerS;
+import net.samagames.hydroangeas.server.games.AbstractGameTemplate;
+
+import java.util.List;
 
 /**
  * This file is a part of the SamaGames Project CodeBase
@@ -57,6 +60,18 @@ public class ShutdownCommand extends AbstractCommand
                     }
                     server.shutdown();
                     instance.getLogger().info(server.getServerName() + " shutdown successfully");
+                    break;
+                case "template":
+                    AbstractGameTemplate template = instance.getTemplateManager().getTemplateByID(args[1]);
+                    if(template == null)
+                    {
+                        instance.getLogger().info("Erreur mauvais nom de template!");
+                        return true;
+                    }
+                    List<MinecraftServerS> serversByTemplate = instance.getClientManager().getServersByTemplate(template);
+                    serversByTemplate.forEach(MinecraftServerS::shutdown);
+
+                    instance.getLogger().info(serversByTemplate.size() + " server shutdown successfully");
                     break;
                 default:
                     showSyntaxe();
